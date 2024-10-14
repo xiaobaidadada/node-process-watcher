b58fc3914522e475ca1cc784572db5cf632250a7
白保祥 <filecat@qq.com>
2024-10-14 20:09:53 +0800

    多余库

--- test/test.ts
+++ test/test.ts
@@ -1,27 +1,6 @@
 import {node_process_monitor} from "../src";
 
-describe('测试',  function() {
-
-    it('监听函数1',  function() {
-        node_process_monitor.on("screen1",(list)=>{
-            console.log(list[0]);
-            node_process_monitor.close("screen1"); // 关闭
-        })
-    });
-    it('监听函数2',  function() {
-        node_process_monitor.on("screen2",(list)=>{
-            console.log(list[1]);
-            node_process_monitor.close("screen2");
-        })
-    });
-    it('切换pids',  function() {
-        node_process_monitor.on("screen3",(list)=>{
-            node_process_monitor.pids("screen3",[
-                list[0].id
-            ])
-            if (list.length === 1) {
-                node_process_monitor.close("screen3");
-            }
-        })
-    });
-});
+node_process_monitor.on("screen1",(list)=>{
+    console.log(list[0]);
+    node_process_monitor.close("screen1"); // 关闭
+})
