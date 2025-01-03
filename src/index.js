b1ffe2b41020f4b7de9eb12693c81daea6fb4018
白保祥 <filecat@qq.com>
2025-01-03 11:57:10 +0800

    1.0.4 prebuildify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-
-const node_process_watcher = require("../build/Release/node-process-watcher.node")
-module.exports.node_process_watcher = node_process_watcher;
+const path = require('path');
+// const node_word_detection = require("../build/Release/node-word-detection.node")
+var binding = require('node-gyp-build')(path.join(__dirname,'..')) // 自动找到 .node
+module.exports.node_process_watcher = binding;
