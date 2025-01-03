05cddf98d013572486b571d851522e9d53c32db7
fielcat <filecat@qq.com>
2025-01-03 12:22:59 +0800

    1.0.5 delete prebuildify

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const path = require('path');
-// const node_word_detection = require("../build/Release/node-word-detection.node")
-var binding = require('node-gyp-build')(path.join(__dirname,'..')) // 自动找到 .node
-module.exports.node_process_watcher = binding;
+
+const node_process_watcher = require("../build/Release/node-process-watcher.node")
+module.exports.node_process_watcher = node_process_watcher;
