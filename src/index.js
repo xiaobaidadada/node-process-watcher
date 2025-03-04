4b8a58c6267678709487dba1d5a831bf48149809
白保祥 <filecat@qq.com>
2025-03-04 17:39:44 +0800

    放弃代码内预构建

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const path = require('path');
-const node_process_watcher = require('node-gyp-build')(path.join(__dirname,'..')) // 自动找到 .node
+
+const node_process_watcher = require("../build/Release/node-process-watcher.node")
 module.exports.node_process_watcher = node_process_watcher;
 
