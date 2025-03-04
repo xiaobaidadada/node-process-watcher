caebef2b50e8a70a4dc2303e466c50ae40bedf08
白保祥 <filecat@qq.com>
2025-03-04 16:40:46 +0800

    新版本与构建

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+const path = require('path');
 const node_process_watcher = require('node-gyp-build')(path.join(__dirname,'..')) // 自动找到 .node
 module.exports.node_process_watcher = node_process_watcher;
 
